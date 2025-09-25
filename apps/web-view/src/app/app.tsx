5bbebfd8b04a8a42c0c6427673c67d3f0e34fd91
Lucas Murta <lucas111murta@gmail.com>
2025-09-25 17:12:40 -0300

    fix: remove nx welcome to my project

--- apps/web-view/src/app/app.tsx
+++ apps/web-view/src/app/app.tsx
@@ -1,13 +1,11 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.scss';
-import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="@epikouros/web-view" />
 
       {/* START: routes */}
       {/* These routes and navigation have been generated for you */}
